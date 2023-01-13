9179811ad1563e4366a513507b05dcd189aef2f2
Ryan Hoover <ryan.hoover@pantheon.io>
2023-01-13 13:09:44 -0600

    Fix linting in db.php

--- web/wp-content/db.php
+++ web/wp-content/db.php
@@ -2,25 +2,24 @@
 /**
  * Plugin Name: MariaDB Optimized Database Class
  *
- * *********************************************************************
- *
  * Ensure this file is symlinked or copied to your wp-content directory to provide
  * required database system variable configuration for MariaDB 10.4
  *
- * *********************************************************************
- *
+ * @package maridb-optimized-database-class
  */
 
-$dbuser      = defined( 'DB_USER' )     ? DB_USER : '';
+$dbuser      = defined( 'DB_USER' ) ? DB_USER : '';
 $dbpassword  = defined( 'DB_PASSWORD' ) ? DB_PASSWORD : '';
-$dbname      = defined( 'DB_NAME' )     ? DB_NAME : '';
-$dbhost      = defined( 'DB_HOST' )     ? DB_HOST : '';
+$dbname      = defined( 'DB_NAME' ) ? DB_NAME : '';
+$dbhost      = defined( 'DB_HOST' ) ? DB_HOST : '';
 $selectivity = defined( 'DB_OPTIMIZER_USE_CONDITION_SELECTIVITY' ) ? DB_OPTIMIZER_USE_CONDITION_SELECTIVITY : 1;
 
 // If selectivity is set to a value other than 1, 2, 3, or 4, overwrite to 1.
-if ( ! is_int( $selectivity) || 1 > $selectivity || 4 < $selectivity  ) {
+if ( ! is_int( $selectivity ) || 1 > $selectivity || 4 < $selectivity ) {
 	$selectivity = 1;
 }
 
+// phpcs:ignore WordPress.WP.GlobalVariablesOverride.OverrideProhibited
 $wpdb = new wpdb( $dbuser, $dbpassword, $dbname, $dbhost );
-$wpdb->query( $wpdb->prepare( "SET optimizer_use_condition_selectivity=%d", $selectivity ) );
+// phpcs:ignore WordPress.DB.DirectDatabaseQuery
+$wpdb->query( $wpdb->prepare( 'SET optimizer_use_condition_selectivity=%d', $selectivity ) );
